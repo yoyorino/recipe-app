7c60cad0218c0be250b92f648892f2d2a7111404
Jovica Spirovski <jovica.spirovski@gmail.com>
2018-01-08 13:40:18 +0100

    testing circle ci

---
+++ src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package guru.springframework.spring5recipeapp.services;
+
+import guru.springframework.spring5recipeapp.domain.Recipe;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+	@InjectMocks
+	private RecipeServiceImpl recipeServiceImpl;
+
+	@Mock
+	private RecipeRepository recipeRepository;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void getRecipes() {
+		Recipe recipe = new Recipe();
+		HashSet<Recipe> recipesData = new HashSet<>();
+		recipesData.add(recipe);
+
+		when(recipeServiceImpl.getRecipes()).thenReturn(recipesData);
+		Set<Recipe> recipes = recipeServiceImpl.getRecipes();
+		assertEquals(recipes.size(), 1);
+		verify(recipeRepository, times(1)).findAll();
+	}
+}
